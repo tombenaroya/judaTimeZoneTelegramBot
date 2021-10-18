f77e5dd00141f8833fdb01746d6cc3817623f510
Tom Ben Aroya <tom.ben1311@gmail.com>
2021-10-18 20:39:59 +0300

    feat(time.js): add logic calculations

--- app.js
+++ app.js
@@ -7,35 +7,41 @@ const port = 3000;
 
 const { api } = require('./api');
 const { parse } = require('node-html-parser');
+const moment = require('moment');
+const { calcMinhaSaturdayEvening, calcSaturdayMinha, calcEveningYetzia } = require('./time');
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-const GROUP_CHAT_ID = -766161903;
+const CHAT_ID = -766161903;
 
 app.get('/', async (req, res) => {
-	const htmlResponse = await api.get();
-	const root = parse(htmlResponse.data);
+  const htmlResponse = await api.get();
+  const root = parse(htmlResponse.data);
 
-	const parasha = root.querySelector('#content_parshaNameAndDetails').innerText;
-	const hadlaka = root.querySelector('#content_hadlaka').innerText.substring(11);
-	const yetzia = root.querySelector('#content_yetzia').innerText.substring(11);
+  const parasha = root.querySelector('#content_parshaNameAndDetails').innerText;
+  const hadlaka = moment(root.querySelector('#content_hadlaka').innerText.substring(11), 'HH:mm');
+  const eveningMinha = calcMinhaSaturdayEvening(hadlaka).format('HH:mm');
+  const saturedayMinha = calcSaturdayMinha(hadlaka).format('HH:mm');
+  const yetzia = moment(root.querySelector('#content_yetzia').innerText.substring(11), 'HH:mm');
+  const eveningYetzia = calcEveningYetzia(yetzia).format('HH:mm');
 
-	const messageToBot = `
+  const messageToBot = `
 	שבת פרשת ${parasha}
 
-	הדלקת נרות ${hadlaka}
+	הדלקת נרות ${hadlaka.format('HH:mm')}
 
-	מנחה ערב שבת XXX ומיד לאחר מכן קבלת שבת
-	שיעור חסידות XXX
-	שחרית XXX
-	מנחה בשבת XXX
-	ערבית צאת שבת XXX
-	מוצאי שבת בשעה ${yetzia}
+	מנחה ערב שבת ${eveningMinha} ומיד לאחר מכן קבלת שבת
+	שיעור חסידות 8:30
+	שחרית 9:00
+	מנחה בשבת ${saturedayMinha}
+	ערבית צאת שבת ${eveningYetzia}
+	מוצאי שבת בשעה ${yetzia.format('HH:mm')}
 	`;
 
-	res.send({ hadlaka, yetzia, parasha, messageToBot });
+  bot.sendMessage(CHAT_ID, messageToBot);
+  res.send();
 });
 
 app.listen(port, () => {
-	console.log(`app is listening on port ${port}`);
+  console.log(`app is listening on port ${port}`);
 });
